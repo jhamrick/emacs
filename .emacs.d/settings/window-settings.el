c8323e2cb60b2e3e75c32274fc2c5b9be67d2e7e
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-03-31 17:38:10 -0700

    Change default window width

--- .emacs.d/settings/window-settings.el
+++ .emacs.d/settings/window-settings.el
@@ -18,29 +18,12 @@
 ; number of characters until the fill column 
 (setq-default fill-column 70)
 
-;; ; specify the fringe width for windows -- this sets both the left and
-;; ; right fringes to 10
-;; (require 'fringe)
-;; ;(fringe-mode 10)
-;; (fringe-mode 0)
-
-;; ; lines which are exactly as wide as the window (not counting the
-;; ; final newline character) are not continued. Instead, when point is
-;; ; at the end of the line, the cursor appears in the right fringe.
-;; (setq overflow-newline-into-fringe 1)
-
 ; each line of text gets one line on the screen (i.e., text will run
 ; off the left instead of wrapping around onto a new line)
 (setq-default truncate-lines 1)
 ; truncate lines even in partial-width windows
 (setq truncate-partial-width-windows 1)
 
-;; ; display line numbers to the right of the window
-;; (global-linum-mode 1)
-;; (setq linum-format " %d ") ; bug fix for Mac OS X
-;; (defun turn-on-linum-mode () (linum-mode 1))
-;; (defun turn-off-linum-mode () (linum-mode -1))
-;; ;(add-hook 'find-file-hook 'turn-on-linum-mode)
 ; show the current line and column numbers in the stats bar as well
 (line-number-mode 1)
 (column-number-mode 1)
@@ -48,7 +31,7 @@
 ; default window width and height
 (defun custom-set-frame-size ()
   (add-to-list 'default-frame-alist '(height . 65))
-  (add-to-list 'default-frame-alist '(width . 102)))
+  (add-to-list 'default-frame-alist '(width . 99)))
 (custom-set-frame-size)
 (add-hook 'before-make-frame-hook 'custom-set-frame-size)
 
