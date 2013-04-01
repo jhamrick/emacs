51cdbed5cba0ab8d095b5d24ca2303b3fc8bfc65
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-03-31 17:13:55 -0700

    Remove lines relating to highlighting the current line

--- .emacs.d/settings/cursor-settings.el
+++ .emacs.d/settings/cursor-settings.el
@@ -2,15 +2,6 @@
 ;;; Cursor ;;;
 ;------------;
 
-; highlight the current line
-;; (add-to-list 'load-path "~/.emacs.d/highlight-current-line-0.57")
-;; (require 'highlight-current-line)
-;; (global-hl-line-mode 1)
-;; (setq highlight-current-line-globally 1)
-;; (setq highlight-current-line-high-faces -1)
-;; (setq highlight-current-line-whole-line -1)
-;; (setq hl-line-face (quote highlight))
-
 ; don't blink the cursor
 (blink-cursor-mode -1)
 
