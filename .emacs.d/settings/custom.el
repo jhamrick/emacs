462dbcd1c918e34c3dc94aa85853693384d21069
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-04-22 16:33:41 -0700

    Fringe, ein, magit face customization

--- .emacs.d/settings/custom.el
+++ .emacs.d/settings/custom.el
@@ -18,11 +18,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "#002b35" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "apple" :family "DejaVu_Sans_Mono"))))
- '(ein:cell-input-area ((t (:background "#073642"))))
+ '(ein:cell-input-area ((t (:background "#042028"))))
+ '(ein:cell-input-prompt ((t (:inherit header-line :background "#002b35" :foreground "#859900" :inverse-video nil :weight bold))))
+ '(ein:cell-output-prompt ((t (:inherit header-line :background "#002b35" :foreground "#dc322f" :inverse-video nil :weight bold))))
  '(font-lock-comment-face ((t (:foreground "#6171c4" :inverse-video nil :underline nil :slant italic :weight normal))))
  '(font-lock-function-name-face ((t (:foreground "#2075c7" :inverse-video nil :underline nil :slant normal :weight bold))))
  '(font-lock-keyword-face ((t (:foreground "#cb4b16" :inverse-video nil :underline nil :slant normal :weight normal))))
  '(font-lock-type-face ((t (:foreground "#d33682" :inverse-video nil :underline nil :slant normal :weight normal))))
+ '(fringe ((t (:background "#002b35" :foreground "#465a61"))))
+ '(magit-item-highlight ((t (:inherit highlight :background "#042028"))))
  '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 210))))
  '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 190))))
  '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 170))))
