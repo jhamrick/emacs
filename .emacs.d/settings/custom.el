db3914d218c173f4304e062c0d14790095c4d747
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-04-22 16:19:05 -0700

    Some more face customization

--- .emacs.d/settings/custom.el
+++ .emacs.d/settings/custom.el
@@ -17,10 +17,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#042028" :foreground "#708183" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "apple" :family "DejaVu_Sans_Mono"))))
+ '(default ((t (:inherit nil :stipple nil :background "#002b35" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "apple" :family "DejaVu_Sans_Mono"))))
  '(ein:cell-input-area ((t (:background "#073642"))))
  '(font-lock-comment-face ((t (:foreground "#6171c4" :inverse-video nil :underline nil :slant italic :weight normal))))
  '(font-lock-function-name-face ((t (:foreground "#2075c7" :inverse-video nil :underline nil :slant normal :weight bold))))
  '(font-lock-keyword-face ((t (:foreground "#cb4b16" :inverse-video nil :underline nil :slant normal :weight normal))))
  '(font-lock-type-face ((t (:foreground "#d33682" :inverse-video nil :underline nil :slant normal :weight normal))))
+ '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 210))))
+ '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 190))))
+ '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 170))))
+ '(markdown-header-face-4 ((t (:inherit markdown-header-face :height 150))))
+ '(markdown-header-face-5 ((t (:inherit markdown-header-face :slant italic :weight bold))))
+ '(markdown-header-face-6 ((t (:inherit markdown-header-face :slant italic :weight normal))))
+ '(markdown-math-face ((t (:inherit font-lock-string-face :foreground "#cb4b16" :slant italic))))
+ '(mode-line ((t (:background "#0a2832" :foreground "#eee8d4" :inverse-video t :box nil :underline nil :slant normal :weight normal))))
  '(py-variable-name-face ((t (:inherit default :foreground "#268bd2")))))
