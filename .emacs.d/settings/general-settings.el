cb0878d4926bb1bd6177a4eff1fcbf6a7c820346
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-02-11 00:18:26 -0800

    Make sure /usr/local/bin is on path before running the shell

--- .emacs.d/settings/general-settings.el
+++ .emacs.d/settings/general-settings.el
@@ -5,7 +5,8 @@
 ; set PATH, because we don't load .bashrc
 ; function from https://gist.github.com/jakemcc/3887459
 (defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
+  (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
+  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo -n $PATH'")))
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
  
