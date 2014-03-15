2c02bb2650ccd35779356339b6c7ea6a52edaf4e
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-03-15 14:53:54 -0700

    Install el-get from emacs init

--- .emacs.d/settings/el-get-settings.el
+++ .emacs.d/settings/el-get-settings.el
@@ -3,9 +3,14 @@
 (unless (file-exists-p elget-path)
   (make-directory elget-path))
 
-; load the el-get plugin
-(include-plugin "el-get")
-(require 'el-get)
+; add el-get to the load path, and install it if it doesn't exist
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
 
 ; packages to install
 (setq 
@@ -25,6 +30,11 @@
                jedi
                ))   
 
+; first enable shallow clone, so we don't need to clone the entire
+; history of every project
+(setq el-get-git-shallow-clone t)
+
+; then intsall!
 (el-get 'sync my-packages)
 
 (provide 'el-get-settings)
