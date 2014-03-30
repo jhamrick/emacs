323f0f4381bcd28e8d7bff193525043a0c2bfe19
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-03-30 12:24:07 -0700

    Update window height

--- .emacs.d/settings/general-settings.el
+++ .emacs.d/settings/general-settings.el
@@ -44,7 +44,7 @@
 
 ; default window width and height
 (defun custom-set-frame-size ()
-  (add-to-list 'default-frame-alist '(height . 53))
+  (add-to-list 'default-frame-alist '(height . 50))
   (add-to-list 'default-frame-alist '(width . 178)))
 (custom-set-frame-size)
 (add-hook 'before-make-frame-hook 'custom-set-frame-size)
