443e234b19907c98368074d227e24dc735ca7278
Chris Gregory <czipperz@gmail.com>
2015-07-10 02:18:52 -0700

    Remove unnecessary variable

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,9 +6,8 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 
-gitExitCode=$?
-if [[ $gitExitCode != 0 ]]; then
-    exit $gitExitCode
+if [[ $? != 0 ]]; then
+    exit $?
 fi
 
 function clean() {
